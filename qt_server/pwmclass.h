35bcc1bb8df2272f54437ecd4ab3933366fb8772
MarioBufu <30401027+MarioBufu@users.noreply.github.com>
2017-10-30 17:25:41 +0200

    Add files via upload

--- qt_server/pwmclass.h
+++ qt_server/pwmclass.h
@@ -1,4 +1,21 @@
 #ifndef PWMCLASS_H
 #define PWMCLASS_H
 
+#include "PinClass.h"
+
+class PWM
+{
+    private:
+        int freq;
+        int gain;
+
+
+    public:
+        void setF(int f);
+        void setG(int g);
+        int getF();
+        int getG();
+        void config();
+};
+
 #endif // PWMCLASS_H
